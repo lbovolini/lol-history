6c675effef4edb2b68600a3c002388aee02242b0
Lucas Bovolini <lbovolini94@gmail.com>
2020-01-20 16:48:43 -0300

    Corrigido collation character set

--- src/main/java/com/github/lbovolini/lol/repository/SummonerRepository.java
+++ src/main/java/com/github/lbovolini/lol/repository/SummonerRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SummonerRepository extends JpaRepository<Summoner, String> {
-    @Query(value = "SELECT * FROM summoner s WHERE lower(replace(s.name, ' ', '')) = lower(replace(?1 , ' ', '')) collate utf8mb4_bin AND s.platform = ?2 ",
+    @Query(value = "SELECT * FROM summoner s WHERE lower(replace(s.name, ' ', '')) = lower(replace(?1 , ' ', '')) collate utf8_bin AND s.platform = ?2 ",
             nativeQuery = true)
     Summoner findByNameAndPlatform(String name, String platform);
 }
